82625f85b575eaeb92f7fdcfe5a17509a93b17ad
darshitbhalodilampros <darshitbhalodia@lampros.tech>
2024-08-01 20:55:06 +0530

    revert back to uniswap permit

--- test/utils/permit2.test.ts
+++ test/utils/permit2.test.ts
@@ -1,6 +1,6 @@
 import { expect } from 'chai'
 import { BigNumber, utils, Wallet } from 'ethers'
-import { PermitSingle } from 'udonswap-permit2'
+import { PermitSingle } from '@uniswap/permit2-sdk'
 import { defaultAbiCoder } from 'ethers/lib/utils'
 import { encodePermit } from '../../src/utils/inputTokens'
 import { RoutePlanner } from '../../src/utils/routerCommands'
